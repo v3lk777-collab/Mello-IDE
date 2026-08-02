5952bef6ee60109a845cff2fb721f5ad7dc49528
v3lk777 <v3lk777@gmail.com>
2026-08-03 00:37:33 +0300

    Update Editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -84,8 +84,110 @@ function CodeEditor({ code, onChange, theme, fontSize, lineHeight, fontFamily } 
                             };
 
                             const suggestions = [
-                                { label: 'start', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'start:\n\t${0}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
-                                { label: 'loop', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'loop:\n\t${0}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                {
+                                    label: 'start',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'start:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'loop',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'loop:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'func',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'func ${1:name}():\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'if',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'if ${1:condition}:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'elif',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'elif ${1:condition}:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'else',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'else:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'while',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'while ${1:condition}:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'for',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'for ${1:item} in ${2:range(0, 10)}:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'repeat',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'repeat ${1:10}:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+                                {
+                                    label: 'every',
+                                    kind: monaco.languages.CompletionItemKind.Snippet,
+                                    insertText: 'every ${1:1s}:\n\t${0}',
+                                    insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                                    range
+                                },
+
+                                { label: 'return', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'return ', range },
+                                { label: 'break', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'break', range },
+                                { label: 'continue', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'continue', range },
+                                { label: 'and', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'and', range },
+                                { label: 'or', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'or', range },
+                                { label: 'not', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'not ', range },
+                                { label: 'in', kind: monaco.languages.CompletionItemKind.Keyword, insertText: 'in', range },
+                                { label: 'range', kind: monaco.languages.CompletionItemKind.Function, insertText: 'range(${1:start}, ${2:end})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'len', kind: monaco.languages.CompletionItemKind.Function, insertText: 'len(${1:value})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+
+                                { label: 'turn_on', kind: monaco.languages.CompletionItemKind.Function, insertText: 'turn_on(${1:pin})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'turn_off', kind: monaco.languages.CompletionItemKind.Function, insertText: 'turn_off(${1:pin})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'toggle', kind: monaco.languages.CompletionItemKind.Function, insertText: 'toggle(${1:pin})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'wait', kind: monaco.languages.CompletionItemKind.Function, insertText: 'wait(${1:1000})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'sleep', kind: monaco.languages.CompletionItemKind.Function, insertText: 'sleep(${1:1000})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'write', kind: monaco.languages.CompletionItemKind.Function, insertText: 'write(${1:pin}, ${2:value})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'read', kind: monaco.languages.CompletionItemKind.Function, insertText: 'read(${1:pin})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'scale', kind: monaco.languages.CompletionItemKind.Function, insertText: 'scale(${1:value}, ${2:fromMin}, ${3:fromMax}, ${4:toMin}, ${5:toMax})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'on_press', kind: monaco.languages.CompletionItemKind.Function, insertText: 'on_press(${1:button}):\n\t${0}', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+
+                                { label: 'serial.start', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.start(${1:9600})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'serial.print', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.print(${1:value})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'serial.println', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.println(${1:value})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'serial.read', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.read()', range },
+                                { label: 'serial.available', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.available()', range },
+                                { label: 'serial.availableForWrite', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.availableForWrite()', range },
+                                { label: 'serial.end', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.end()', range },
+                                { label: 'serial.find', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.find(${1:text})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'serial.findUntil', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.findUntil(${1:text}, ${2:until})', insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, range },
+                                { label: 'serial.waitUntilSend', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.waitUntilSend()', range },
+                                { label: 'serial.parseFloat', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.parseFloat()', range },
+                                { label: 'serial.parseInt', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.parseInt()', range },
+                                { label: 'serial.peek', kind: monaco.languages.CompletionItemKind.Method, insertText: 'serial.peek()', range },
                             ];
 
                             return { suggestions };
